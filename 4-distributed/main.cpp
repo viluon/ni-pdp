fa55f8ce50029e2c8816dba08dd338a3036e9cb6
Andrew Kvapil <viluon@pm.me>
2022-04-28 22:56:42 +0200

    Finalise distributed solution

--- 4-distributed/main.cpp
+++ 4-distributed/main.cpp
@@ -7,6 +7,7 @@
 #include <algorithm>
 #include <numeric>
 #include <deque>
+#include <chrono>
 
 namespace mpi = boost::mpi;
 
@@ -23,7 +24,6 @@ typedef  int_fast8_t   i8;
 using namespace std;
 
 auto dummy = vector<vector<u32>>();
-constexpr u32 a = 15;
 
 vector<vector<u32>> load_input() {
     vector<vector<u32>> graph;
@@ -226,8 +226,8 @@ void solve(Sln* result, u32 a, const vector<vector<u32>>& graph, const Sln& sln,
     }
 }
 
-deque<pair<u16, Sln>> gen_initial_configurations(vector<vector<u32>>& graph) {
-    constexpr u8 p = 4;
+deque<pair<u16, Sln>> gen_initial_configurations(u16 a, vector<vector<u32>>& graph) {
+    const u16 p = 3 * a;
     constexpr u8 z = 15; // found optimal with Hyperfine
     auto q = deque<pair<u16, Sln>>();
     q.emplace_back(make_pair(0, Sln(&graph)));
@@ -274,7 +274,9 @@ private:
 };
 
 // invariant: only called when the queue is non-empty
-mpi::request send_configurations(mpi::communicator& world, i32 dest, deque<pair<u16, Sln>>& queue) {
+mpi::request __attribute__((warn_unused_result)) send_configurations(
+    mpi::communicator& world, i32 dest, deque<pair<u16, Sln>>& queue
+) {
     WorkData batch;
     for (u32 i = 0; i < batch_size; i++) {
         if (queue.empty()) {
@@ -287,10 +289,10 @@ mpi::request send_configurations(mpi::communicator& world, i32 dest, deque<pair<
     return world.isend(dest, TAG_WORK, batch);
 }
 
-deque<pair<u16, Sln>> init_slaves(mpi::communicator& world, vector<vector<u32>>& graph) {
-    auto queue = gen_initial_configurations(graph);
+deque<pair<u16, Sln>> init_slaves(mpi::communicator& world, u16 a, vector<vector<u32>>& graph) {
+    auto queue = gen_initial_configurations(a, graph);
     vector<mpi::request> requests;
-    requests.reserve(3 * world.size());
+    requests.reserve(2 * world.size());
     for (i32 dest = 1; dest < world.size(); dest++) {
         // send the graph and the initial configurations to the workers
         requests.emplace_back(world.isend(dest, TAG_GRAPH, graph));
@@ -301,10 +303,14 @@ deque<pair<u16, Sln>> init_slaves(mpi::communicator& world, vector<vector<u32>>&
     return queue;
 }
 
-void master(mpi::communicator& world) {
+void master(mpi::communicator& world, u16 a) {
+    auto start = chrono::steady_clock::now();
     auto graph = load_input();
-    cout << "loaded graph of n = " << graph.size() << endl;
-    auto queue = init_slaves(world, graph);
+    chrono::duration<double> input_load_time = chrono::steady_clock::now() - start;
+    cout << "loaded graph of n = " << graph.size() << " in " << input_load_time.count() << " s" << endl;
+
+    start = chrono::steady_clock::now();
+    auto queue = init_slaves(world, a, graph);
 
     Sln best = Sln(&graph);
     u32 working_slaves = world.size() - 1;
@@ -327,7 +333,9 @@ void master(mpi::communicator& world) {
     for (u32 i = 0; i < graph.size(); i++) {
         cout << (u32)best.assignment[i] << " ";
     }
-    cout << endl;
+
+    chrono::duration<double> total_time = chrono::steady_clock::now() - start;
+    cout << endl << "took " << total_time.count() << " s" << endl;
 }
 
 // Listen for TAG_WORK_NODE_VECTOR, TAG_WORK_SLN_VECTOR, and TAG_TERMINATE
@@ -338,6 +346,7 @@ bool wait_for_work(mpi::communicator& world, WorkData& work) {
         world.irecv(0, TAG_TERMINATE),
         world.irecv(0, TAG_WORK, work),
     };
+    cout << "slave " << world.rank() << ": waiting for work" << endl;
     auto [status, req_ptr] = mpi::wait_any(reqs, reqs + 2);
 
     if (req_ptr == &reqs[0]) {
@@ -351,21 +360,14 @@ bool wait_for_work(mpi::communicator& world, WorkData& work) {
     }
 }
 
-void slave(mpi::communicator& world) {
+void slave(mpi::communicator& world, u16 a, u16 n_threads) {
     // receive the graph
     vector<vector<u32>> graph;
     world.recv(0, TAG_GRAPH, graph);
 
-    while (true) {
-        cout << "slave " << world.rank() << ": waiting for work" << endl;
-        WorkData work;
-
-        if (!wait_for_work(world, work)) {
-            break;
-        }
-
+    for (WorkData work; wait_for_work(world, work);) {
         // process the batch
-        #pragma omp parallel for schedule(dynamic)
+        #pragma omp parallel for schedule(dynamic) num_threads(n_threads)
         for (u32 i = 0; i < work.node_ids.size(); i++) {
             auto node = work.node_ids[i];
             auto sln = work.configs[i];
@@ -379,15 +381,23 @@ void slave(mpi::communicator& world) {
     }
 }
 
-int main() {
+int main(int argc, char** argv) {
     mpi::environment env;
     mpi::communicator world;
 
+    if (argc < 3) {
+        cout << "usage: " << argv[0] << " <a> <n_threads>" << endl;
+        return 1;
+    }
+
+    u16 a = atoi(argv[1]);
+    u16 n_threads = atoi(argv[2]);
+
     if (world.rank() == 0) {
-        master(world);
+        master(world, a);
         cout << "master " << world.rank() << " finished" << endl;
     } else {
-        slave(world);
+        slave(world, a, n_threads);
         cout << "slave " << world.rank() << " finished" << endl;
     }
 
